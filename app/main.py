87fc7e38501ac2596b7fa8a7a71a4ed250569ea5
Shomiya Chaturvedi <shomiyachaturvedi@rootedgrit.local>
2026-08-01 23:24:31 +0530

    second commit

--- app/main.py
+++ app/main.py
@@ -4,4 +4,17 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "E-commerce API is running"}
+    return {"message": "Welcome to ECommerce API!"}
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int):
+    return {
+        "product_id": product_id,
+        "name": "Sample Product",
+        "price": 99
+    }
+
+
+@app.get("/search")
+def search_products(category: str):
+    return {"category": category}
